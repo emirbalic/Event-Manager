4fb9458e9f4087c3da36fe8d1f89b6566ad829f2
Emir Balic <emirbalic@gmail.com>
2020-05-28 13:02:26 +0200

    Photo Uploading

---
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -0,0 +1,172 @@
+import React, { useState, useEffect, Fragment } from 'react'; // , {Component}
+// useEffect - func run after render is commited to the screen and clean up
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import {
+  // Image,
+  Segment,
+  Header,
+  Divider,
+  Grid,
+  Button,
+  // Card,
+} from 'semantic-ui-react';
+import DropzoneInput from './DropzoneInput';
+import CropperInput from './CropperInput';
+import {
+  uploadProfileImage,
+  deletePhoto,
+  setMainPhoto,
+} from '../../userActions';
+import { toastr } from 'react-redux-toastr';
+import UserPhotos from './UserPhotos';
+
+const query = ({ auth }) => {
+  return [
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{ collection: 'photos' }],
+      storeAs: 'photos',
+    },
+  ];
+};
+
+// <<actions>>
+const mapDispatchToProps = {
+  uploadProfileImage,
+  deletePhoto,
+  setMainPhoto,
+};
+
+// maybe better () then return
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+    photos: state.firestore.ordered.photos,
+    loading: state.async.loading,
+  };
+};
+
+// class PhotosPage extends Component {
+// ... but in order to use hooks ...
+const PhotosPage = ({
+  uploadProfileImage,
+  photos,
+  profile,
+  deletePhoto,
+  setMainPhoto,
+  loading,
+}) => {
+  // render() {   // == no render in functional component using hooks so...
+  const [files, setFiles] = useState([]);
+  // const [cropResult, setCropResult] = useState([]);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
+
+  const handleUploadImage = async () => {
+    try {
+      // console.log('im in handleUploadImage!!');
+      await uploadProfileImage(image, files[0].name);
+      handleCancelCrop();
+      toastr.success('Success', 'Photo has been uploaded');
+    } catch (error) {
+      console.log(error);
+      toastr.error('Ooops', 'Something went wrong');
+    }
+  };
+  const handleCancelCrop = () => {
+    // console.log('im in handleCancelCrop!!');
+
+    setFiles([]);
+    setImage(null);
+  };
+
+  const handleSetMainPhoto = async (photo) => {
+    //
+    try {
+      await setMainPhoto(photo);
+    } catch (error) {
+      toastr.error('Oops', error.message);
+    }
+  };
+
+  const handleDeletePhoto = async (photo) => {
+    try {
+      await deletePhoto(photo);
+    } catch (error) {
+      toastr.error('Oops', error.message);
+    }
+  };
+  return (
+    <Segment>
+      <Header dividing size='large' content='Your Photos' />
+      <Grid>
+        <Grid.Row />
+        <Grid.Column width={4}>
+          <Header color='teal' sub content='Step 1 - Add Photo' />
+          <DropzoneInput setFiles={setFiles} />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color='teal' content='Step 2 - Resize image' />
+          {files.length > 0 && (
+            <CropperInput setImage={setImage} imagePreview={files[0].preview} />
+          )}
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color='teal' content='Step 3 - Preview & Upload' />
+          {files.length > 0 && (
+            <Fragment>
+              <div
+                className='img-preview'
+                // src={files[0].preview}
+                style={{
+                  minHeight: '200px',
+                  minWidth: '200px',
+                  overflow: 'hidden',
+                }}
+              />
+              <Button.Group>
+                <Button
+                  loading={loading}
+                  onClick={handleUploadImage}
+                  style={{ width: '100px' }}
+                  positive
+                  icon='check'
+                />
+                <Button
+                  disabled={loading}
+                  onClick={handleCancelCrop}
+                  style={{ width: '100px' }}
+                  icon='close'
+                />
+              </Button.Group>
+            </Fragment>
+          )}
+        </Grid.Column>
+      </Grid>
+
+      <Divider />
+      <UserPhotos
+        setMainPhoto={handleSetMainPhoto}
+        photos={photos}
+        profile={profile}
+        deletePhoto={handleDeletePhoto}
+      />
+    </Segment>
+  );
+};
+// compose () makes HOFunctions a little bit nieter (down not right)
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect((auth) => query(auth))
+)(PhotosPage);
--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -1,4 +1,10 @@
-import { toastr } from "react-redux-toastr";
+import { toastr } from 'react-redux-toastr';
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from '../async/asyncActions';
+import cuid from 'cuid';
 
 export const updateProfile = (user) => async (
   dispatch,
@@ -7,7 +13,7 @@ export const updateProfile = (user) => async (
 ) => {
   const firebase = getFirebase();
   // <<< spread operator is excluding fields that are not necessary and updatedUser is fields from redux form minus these two >>> //
-  const {isLoaded, isEmpty, ...updatedUser} = user;
+  const { isLoaded, isEmpty, ...updatedUser } = user;
   toastr.success('Success', 'Your profile has been updated');
   try {
     await firebase.updateProfile(updatedUser);
@@ -15,3 +21,87 @@ export const updateProfile = (user) => async (
     console.log(error);
   }
 };
+
+export const uploadProfileImage = (file, imageName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const imageName = cuid();
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: imageName,
+  };
+
+  try {
+    dispatch(asyncActionStart());
+    // upload photo
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get Url
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+    // get userdoc
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // check if user has a photo
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL,
+      });
+      // optional
+      await user.updateProfile({
+        photoURL: downloadURL,
+      });
+    }
+    await firestore.add(
+      {
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{ collection: 'photos' }],
+      },
+      {
+        name: imageName,
+        url: downloadURL,
+      }
+    );
+    dispatch(asyncActionFinish());
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
+  }
+};
+
+export const deletePhoto = (photo) => 
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+
+    try {
+      await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`);
+      await firestore.delete({
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{ collection: 'photos', doc: photo.id }]
+      })
+    } catch (error) {
+      console.log(error);
+      throw new Error ('Problem deleting the photo');
+    }
+  }
+
+  export const setMainPhoto = photo =>
+    async (dispatch, getState, {getFirebase}) => {
+      const firebase = getFirebase();
+      try {
+        return await firebase.updateProfile({
+          photoURL: photo.url
+        });
+      } catch (error) {
+        console.log(error);
+        throw new Error('Problem setting main photo');
+      }
+    }
+
+
